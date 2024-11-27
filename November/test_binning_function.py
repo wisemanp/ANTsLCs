af8e2b825b8b80149a587ee3f4f74701452820dc
laureneastman <laureneastman14@gmail.com>
2024-11-27 16:13:46 +0000

    Adapted the code to fit the fact that I updated the bin_lc function to take in  a list of ANT light curve dataframes and output a list of binned up ANT dataframes. By doing this, I noticed that I was getting runtimewarnings from the weighted mean funciton because I have some datapoints for some of the ANTs where magerr = 0.0, so I have a little commented out section where I am looking at which ANTs have this issue and what this data looks like.

--- November/test_binning_function.py
+++ November/test_binning_function.py
@@ -14,18 +14,29 @@ lc_df_list, transient_names, list_of_bands = load_ANT_data()
 # produce a list of ANT light curve dataframes with the addition of rest frame luminosity, its error and emitted wavelength columns
 mod_lc_df_list = ANT_data_L_rf(lc_df_list, transient_names, ANT_redshift_dict, ANT_luminosity_dist_cm_dict, band_ZP_dict, band_obs_centwl_dict) 
 
+# bin the light curves
+MJD_binsize = 1
+binned_ANT_lc_list = bin_lc(mod_lc_df_list, MJD_binsize, drop_na_bins = True)
+"""  this commented out snippet just shows the problematic ANT datafiles, since some of them have magerr = 0.0 which gives the runtimewarning for the weighted mean function
+print()
+print()
+TEST_PROBLEM_IDX = [14, 15, 16]
+for k in TEST_PROBLEM_IDX:
+    print(f'{k}: {transient_names[k]}')
+    problem_df = lc_df_list[k].copy()
+    print(problem_df[problem_df['L_rf_err'] == 0.0])
+    print()
+
+ """
 # choose an ANT to test the binning funciton on
-ANT = 'ZTF20acvfraq'
-ANT = 'ZTF20abodaps'
+#ANT = 'ZTF20acvfraq'
+#ANT = 'ZTF20abodaps'
 ANT = 'ZTF22aadesap'
 idx = transient_names.index(ANT)
 ANT_lc = mod_lc_df_list[idx].copy()
 ANT_name = transient_names[idx]
 ANT_bands = list_of_bands[idx]
-
-# bin the light curve
-MJD_binsize = 1
-binned_ANT_lc = bin_lc(ANT_lc, MJD_binsize, drop_na_bins = True)
+binned_ANT_lc = binned_ANT_lc_list[idx].copy()
 
 
 # plot the binned light curve on top of the original light curve to check 
