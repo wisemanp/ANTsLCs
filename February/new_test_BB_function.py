7deea063bbd512bf9f0c0d88ebcd42a364a25b9c
laureneastman <laureneastman14@gmail.com>
2025-02-11 18:25:13 +0000

    removed the fit_BB function as we've now turned it into a class

--- February/new_test_BB_function.py
+++ February/new_test_BB_function.py
@@ -17,212 +17,6 @@ from functions import load_interp_ANT_data, blackbody, chisq
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-def fit_BB_across_lc(interp_df, curvefit, brute, brute_gridsize, brute_param_sigma = 1, BB_R_min = 1e13, BB_R_max = 1e19, BB_T_min = 1e3, BB_T_max = 1e7):
-    """
-
-
-    INPUTS
-    ---------------
-    interp_df: (dataframe) the ANT's dataframe containing a light curve which has been interpolated using a polynomial fit to each band. 
-        Each ANT had a chosen reference band. At the MJD values present in the reference band's real data, the polyfit for all other bands
-        were evaluated (provided that we aren't extrapolating). This means that if there was a band which had data at the min and max 
-        MJDs of the flare, there will be interpolated data for this band across the whole flare light curve, whereas if there is a band
-        which only has data on the plateau of the light curve, this band will only have interpolated data within this region, at
-        the MJD values of the reference band's data. This means that we don't need to bin the light curve in order to take the data
-        for the blackbody fit, we can take each MJD present within this dataframe and take whatever band is present at this MJD as
-        the data for the BB fit. So, we can fit a BB curve for each MJD within this dataframe, as long as it has >2 bands present. Prior
-        to being interpolated, the ANT data should (ideally) be binned into small bins like 1 day, meaning that we will only have 0 or 1 datapoint 
-        per per band per MJD value (this is only really relevant for the reference band, though, since the interpolation function ensures either 0 or 1
-        value per band per MJD value for the interpolated data, since the polynomials are single-valued for any given MJD value)
-
-    curvefit: (bool) if True, the BB fit will be tried using scipy's curve_fit. If False, no curve_fit calculation will be tried
-
-    brute: (bool) if True, the BB fit will be tried using the brute force method (manually creating a grid of trial parameter values and minimising the chi squared). If 
-        False, no brute force calculation will be tried
-
-    brute_gridsize: (int) the number of trial values of R and T that will be tried in the brute force method. The number of trial values of R and T form a 2D grid of parameters and
-    each combination of R and T will be tried in the BB fit.
-
-    brute_param_sigma: (float or int) the number of sigma that the brute force method will use to calculate the error on the BB fit parameters. 
-
-    BB_R_min: (float) the minimum value of the radius parameter that will be tried in the BB fit. 
-
-    BB_R_max: (float) the maximum value of the radius parameter that will be tried in the BB fit.
-
-    BB_T_min: (float) the minimum value of the temperature parameter that will be tried in the BB fit.
-
-    BB_T_max: (float) the maximum value of the temperature parameter that will be tried in the BB fit.
-
-
-    RETURNS
-    ---------------
-
-    """
-    # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    # scale down the radius values to explore - this requires scaling down the rest frame luminosity by (R_scalefactor)**2 because L ~ R^2 
-    R_scalefactor = 1e-16
-    L_scalefactor = (R_scalefactor)**2
-    interp_df['L_rf_scaled'] = interp_df['L_rf'] * L_scalefactor # scale down the rest frame luminosity and its error 
-    interp_df['L_rf_err_scaled'] = interp_df['L_rf_err'] * L_scalefactor
-    BB_R_min_sc = BB_R_min * R_scalefactor # scaling down the bounds for the radius parameter space
-    BB_R_max_sc = BB_R_max * R_scalefactor
-    interp_df['em_cent_wl_cm'] = interp_df['em_cent_wl'] * 1e-8 # the blackbody function takes wavelength in centimeters. 1A = 1e-10 m.     1A = 1e-8 cm
-
-    brute_dchi = (brute_param_sigma)**2 # the number of sigma that the brute force method will use to calculate the error on the BB fit parameters. look to Christians stats module if confused about this
-
-
-    # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    # iterate through each value of MJD within the dataframe and see if we have enough bands to take a BB fit to it 
-    mjd_values = interp_df['MJD'].unique() 
-    columns = ['MJD', 'no_bands', 'cf_T_K', 'cf_T_err_K', 'cf_R_cm', 'cf_R_err_cm', 'cf_covariance', 'cf_red_chi', 'cf_chi_sigma_dist', 'red_chi_1sig', 'brute_T_K', 'brute_R_cm', 'brute_red_chi', 'brute_chi_sigma_dist']
-    BB_fit_results = pd.DataFrame(columns = columns)
-
-    for MJD in tqdm(mjd_values, desc = 'Progress BB fitting each MJD value', total = len(mjd_values), leave = False):
-        MJD_df = interp_df[interp_df['MJD'] == MJD].copy() # THERE COULD BE FLOATING POINT ERRORS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        MJD_no_bands = len( MJD_df['band'].unique() ) # the number of bands (and therefore datapoints) we have available at this MJD for the BB fit
-        df_row_index = len(BB_fit_results['MJD'])
-        BB_result_row = np.zeros(len(columns))
-        BB_result_row[:] = np.nan # set all values to nan for now, then overwrite them if we have data for thsi column, so that if (e.g.) brute = False, then the brute columns would contain nan values
-        BB_result_row[0:2] = [MJD, MJD_no_bands] # the first column in the dataframe is MJD, so set the first value in the row as the MJD
-       
-        if MJD_no_bands <= 1: # if there's <= 2 bands present for a particular MJD, don't bother fitting a BB 
-            BB_result_row[2: -1] = np.nan
-            BB_fit_results.loc[df_row_index] = BB_result_row # adding the array of results from this MJD to the BB results dataframe
-            continue
-
-        # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        # because of the way that interp_df is set up, we don't need to bin by MJD for BB data and we only have 0 or 1 datapoint per band per MJD value, so no need to average the data for ecah band. 
-        # so, MJD_df actually contains all of the necessary data for the BB fit. 
-        # curve_fit fitting 
-        if curvefit == True:
-            try:
-                popt, pcov = opt.curve_fit(blackbody, xdata = MJD_df['em_cent_wl_cm'], ydata = MJD_df['L_rf_scaled'], sigma = MJD_df['L_rf_err_scaled'], absolute_sigma = True, 
-                                        bounds = (np.array([BB_R_min_sc, BB_T_min]), np.array([BB_R_max_sc, BB_T_max])))
-                sc_cf_R, cf_T = popt
-                sc_cf_R_err = np.sqrt(pcov[0, 0])
-                cf_T_err = np.sqrt(pcov[1, 1])
-                cf_R = sc_cf_R / R_scalefactor
-                cf_R_err = sc_cf_R_err / R_scalefactor
-                cf_covariance = pcov[1,0]
-
-
-                # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                # calculate the reduced chi squared of the curve_fit result
-                BB_sc_L_chi = [blackbody(wl_cm, sc_cf_R, cf_T) for wl_cm in MJD_df['em_cent_wl_cm']] # evaluating the BB model from curve_fit at the emitted central wavelengths present in our data to use for chi squared calculation
-                cf_red_chi, red_chi_1sig = chisq(y_m = BB_sc_L_chi, y = MJD_df['L_rf_scaled'], yerr = MJD_df['L_rf_err_scaled'], M = 2, reduced_chi = True)
-                cf_chi_sigma_dist = abs(1 - cf_red_chi)/red_chi_1sig
-
-                # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                # add the result to the results row which will be appended to the results dataframe
-                #print(cf_T, cf_T_err, cf_R, cf_R_err, cf_red_chi, cf_chi_sigma_dist, red_chi_1sig)
-                BB_result_row[2:10] = [cf_T, cf_T_err, cf_R, cf_R_err, cf_covariance, cf_red_chi, cf_chi_sigma_dist, red_chi_1sig]
-
-            except RuntimeError:
-                print(f'{Fore.RED} WARNING - Curve fit failed for MJD = {MJD} {Style.RESET_ALL}')
-                BB_result_row[2:10] = np.nan
-
-
-        # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        if brute == True:
-            # creating the values of R and T that we will try
-            # the number of R and T values to trial in the grid. The combinations of R and T form a 2D grid, so the number of R and T values that we try give the side lengths of the grid
-            sc_R_values = np.logspace(np.log10(BB_R_min_sc), np.log10(BB_R_max_sc), brute_gridsize)
-            T_values = np.logspace(np.log10(BB_T_min), np.log10(BB_T_max), brute_gridsize)
-
-            wavelengths = MJD_df['em_cent_wl_cm'].to_numpy() # the emitted central wavelengths of the bands present at this MJD value
-            L_rfs = MJD_df['L_rf_scaled'].to_numpy() # the scaled rest frame luminosities of the bands present at this MJD value
-            L_rf_errs = MJD_df['L_rf_err_scaled'].to_numpy() # the scaled rest frame luminosity errors of the bands present at this MJD value
-
-            # create a 3D array of the blackbody luminosities for each combination of R and T. This is done by broadcasting the 1D arrays of wavelengths, R values and T values
-            # the 3D array will have dimensions (len(wavelengths), len(R_values), len(T_values)) and will contain the blackbody luminosities for each combination of R and T for each wavelength value
-            BB_L_sc = blackbody(wavelengths[:, np.newaxis, np.newaxis], sc_R_values[np.newaxis, :, np.newaxis], T_values[np.newaxis, np.newaxis, :]) # the calculated value of scaled rest frame luminosity using this value of T and scaled R
-
-            # calculate the chi squared of the fit
-            chi = np.sum((L_rfs[:, np.newaxis, np.newaxis] - BB_L_sc)**2 / L_rf_errs[:, np.newaxis, np.newaxis]**2, axis = 0) # the chi squared values for each combination of R and T
-            min_chi = np.min(chi) # the minimum chi squared value
-            row, col = np.where(chi == min_chi) # the row and column indices of the minimum chi squared value
-
-            if (len(row) == 1) & (len(col) == 1): 
-                r = row[0]
-                c = col[0]
-                brute_T = T_values[c] # the parameters which give the minimum chi squared
-                brute_R = sc_R_values[r] / R_scalefactor
-                N_M = len(MJD_df['band']) - 2
-
-                if N_M > 0: # this is for when we try to 'fit' a BB to 2 datapoints, since we have 2 parameters, we can't calculate a reduced chi squared value......
-                    brute_red_chi = min_chi / N_M
-                    red_chi_1sig = np.sqrt(2/N_M)
-                    brute_chi_sigma_dist = abs(1 - brute_red_chi) / red_chi_1sig
-
-                else:
-                    brute_red_chi = np.nan
-                    red_chi_1sig = np.nan
-                    brute_chi_sigma_dist = np.nan
-
-            else:
-                print()
-                print(f"{Fore.RED} WARNING - MULTIPLE R AND T PARAMETER PAIRS GIVE THIS MIN CHI VALUE. MJD = {MJD_df['MJD'].iloc[0]} \n Ts = {[T_values[r] for r in row]}, Rs = {[sc_R_values[c]/R_scalefactor for c in col]}")
-                print(f"Chi values = {chi[row, col]} {Style.RESET_ALL}")
-                print()
-
-            # calculate the error on the parameters using the brute force method
-            row_idx, col_idx = np.nonzero(chi <= (min_chi + brute_dchi)) # the row and column indices of the chi squared values which are within the error of the minimum chi squared value
-            delchi_T = T_values[col_idx] # the values of T which are within the error of the minimum chi squared value
-            delchi_sc_R = sc_R_values[row_idx]  # the values of R which are within the error of the minimum chi squared value
-
-            brute_T_err_upper = np.max(delchi_T) - brute_T # the upper error on the temperature parameter
-            brute_T_err_lower = brute_T - np.min(delchi_T) # the lower error on the temperature parameter
-            brute_R_err_upper = np.max(delchi_sc_R) / R_scalefactor - brute_R # the upper error on the radius parameter
-            brute_R_err_lower = brute_R - np.min(delchi_sc_R) / R_scalefactor # the lower error on the radius parameter
-
-            #print(f'brute T err upper {brute_T_err_upper:.3e} brute T err lower {brute_T_err_lower:.3e} cf T err {cf_T_err:.3e}')
-            #print(f'brute R err upper {brute_R_err_upper:.3e} brute R err lower {brute_R_err_lower:.3e} cf R err {cf_R_err:.3e}')
-            #print()
-            
-
-            # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-            # add the result to the results row which will be appended to the results dataframe
-            BB_result_row[9:14] = [red_chi_1sig, brute_T, brute_R, brute_red_chi, brute_chi_sigma_dist]
-
-
-
-        BB_fit_results.at[df_row_index] = BB_result_row # adding the array of results from this MJD to the BB results dataframe
-
-        
-    return BB_fit_results
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class fit_BB_across_lightcurve:
     def __init__(self, interp_df, curvefit, brute, brute_gridsize, brute_param_sigma = 1, BB_R_min = 1e13, BB_R_max = 1e19, BB_T_min = 1e3, BB_T_max = 1e7):
         """
@@ -238,7 +32,8 @@ class fit_BB_across_lightcurve:
             the data for the BB fit. So, we can fit a BB curve for each MJD within this dataframe, as long as it has >2 bands present. Prior
             to being interpolated, the ANT data should (ideally) be binned into small bins like 1 day, meaning that we will only have 0 or 1 datapoint 
             per per band per MJD value (this is only really relevant for the reference band, though, since the interpolation function ensures either 0 or 1
-            value per band per MJD value for the interpolated data, since the polynomials are single-valued for any given MJD value)
+            value per band per MJD value for the interpolated data, since the polynomials are single-valued for any given MJD value). 
+            columns: MJD, L_rf, L_rf_err, band, em_cent_wl
 
         curvefit: (bool) if True, the BB fit will be tried using scipy's curve_fit. If False, no curve_fit calculation will be tried
 
@@ -376,7 +171,6 @@ class fit_BB_across_lightcurve:
         #print(f'brute R err upper {brute_R_err_upper:.3e} brute R err lower {brute_R_err_lower:.3e} cf R err {cf_R_err:.3e}')
         #print()
         
-        param_grid = np.meshgrid(R_values, T_values)
         # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         # add the result to the results row which will be appended to the results dataframe
         self.BB_fit_results.loc[MJD, self.columns[9:14]] = [red_chi_1sig, brute_T, brute_R, brute_red_chi, brute_chi_sigma_dist]#, param_grid, chi] # add parameter grid and chi grid
@@ -401,6 +195,11 @@ class fit_BB_across_lightcurve:
 
         return self.BB_fit_results
     
+
+
+    #def plot_individual_BB_fits(self):
+
+
 
 
 
@@ -434,14 +233,17 @@ interp_df_list, transient_names, list_of_bands = load_interp_ANT_data()
 
 
 
-for idx in range(11):
-#for idx in [5]:
+#for idx in range(11):
+for idx in [10]:
 
     ANT_name = transient_names[idx]
     interp_lc= interp_df_list[idx]
     ANT_bands = list_of_bands[idx]
     print()
     print(ANT_name)
+    
+    #if idx == 10:
+    #    interp_lc = interp_lc[~interp_lc['band'].isin(['UVOT_B', 'UVOT_U', 'UVOT_UVM2', 'UVOT_UVW1', 'UVOT_UVW2', 'UVOT_V'])]
     
 
     BB_curvefit = True
@@ -450,7 +252,7 @@ for idx in range(11):
     BB_fitting = fit_BB_across_lightcurve(interp_lc, curvefit = BB_curvefit, brute = BB_brute, brute_gridsize = 1000)
     BB_fit_results = BB_fitting.run_BB_fit()
     BB_2dp = BB_fit_results[BB_fit_results['no_bands'] == 2] # the BB fits for the MJDs which only had 2 bands, so we aren't really fitting, more solving for the BB R and T which perfectly pass through the data points
-    print(BB_fit_results)
+    #print(BB_fit_results)
     print()
     
 
