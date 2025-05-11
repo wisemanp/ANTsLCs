2bfe5eadf4a4aab9f6d6140d57b2136fd9749c86
laureneastman <laureneastman14@gmail.com>
2025-05-11 22:24:06 +0100

    Irrelevant changes

--- February/new_test_BB_function.py
+++ February/new_test_BB_function.py
@@ -64,7 +64,7 @@ SED_plots = 'usual'#'compare_SEDs'
 
 if SED_plots == 'usual':
     #for idx in range(len(transient_names)):
-    for idx in [3]:
+    for idx in [13]:
 
         ANT_name = transient_names[idx]
         interp_lc= interp_df_list[idx]
@@ -80,8 +80,8 @@ if SED_plots == 'usual':
         BB_curvefit = True
         BB_brute = True
         #SED_type = 'single_BB'
-        #SED_type = 'double_BB'
-        SED_type = 'power_law'
+        SED_type = 'double_BB'
+        #SED_type = 'power_law'
         #SED_type = 'best_SED'
         UVOT_guided_fitting = True # if True, will call run_UVOT_guided_SED_fitting_process() instead of run_SED_fitting process(). When an ANT has UVOT on the rise/peak, will use the UVOT SED fit results to constrain the parameter space to search for the nearby non-UVOT SED fits
         UVOT_guided_err_scalefactor = 0.1 
